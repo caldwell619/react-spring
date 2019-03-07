7286bbe58f1b8ee23bd059eead904865245f7f8e
caldwell619 <caldwell8814@gmail.com>
2019-03-07 17:25:11 -0600

    initial commit to remote

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  componentDidMount(){
+        axios.get("/api/test").then(res => console.log(res))
+            .catch(error => console.log(error))
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <h1>Doing stuff</h1>
     );
   }
 }
--- src/main/java/com/reactspring/ReactSpringApplication.java
+++ src/main/java/com/reactspring/ReactSpringApplication.java
@@ -1,11 +1,10 @@
-package com.reactspring.reactspring;
+package com.reactspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReactSpringApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ReactSpringApplication.class, args);
     }
